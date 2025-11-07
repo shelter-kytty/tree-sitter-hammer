03ce0e711cb43b8e136d97c496448e7b4b57940e
Kytty <152751739+shelter-kytty@users.noreply.github.com>
2025-11-07 14:21:15 +1030

    Incremental
    Currently attempting to fix function application

--- grammar.js
+++ grammar.js
@@ -10,9 +10,11 @@
 module.exports = grammar({
   name: "hammer",
 
-  conflicts: ($) => [[$.binary_expression, $.deconstruct]],
+  // conflicts: ($) => [[$.binary_expression, $.decons]],
 
   word: ($) => $.identifier,
+
+  externals: ($) => [$.application_params],
 
   extras: ($) => [
     /\s/, //whitespace
@@ -20,69 +22,83 @@ module.exports = grammar({
   ],
 
   rules: {
-    source_file: ($) => repeat($._definition),
+    source_file: ($) => repeat($._statememt),
 
-    _definition: ($) =>
-      choice($.function_definition, $.name_definition, $.expression),
+    _statememt: ($) => choice($.function, $.name_definition, $._expression),
 
-    function_definition: ($) =>
-      seq(
-        field("name", $.identifier),
-        field("parameters", $.parameter_list),
-        field("body", $.block),
+    function: ($) =>
+      prec(
+        1,
+        seq(
+          field("name", $.identifier),
+          field("parameters", $.parameter_list),
+          field("body", $._expression),
+        ),
       ),
 
     parameter_list: ($) => seq(":", repeat($.identifier), "="),
 
-    block: ($) => seq("{", repeat($._definition), "}"),
-
-    name_definition: ($) => seq($.identifier, "=", $.expression),
-
-    expression: ($) =>
-      choice(
-        $.block,
-        $.if_statement,
-        $.lambda,
-        $.binary_expression,
-        $.unary_expression,
-        $.literal,
-        $.identifier,
+    name_definition: ($) =>
+      prec.right(
+        1,
+        seq($.identifier, repeat(seq(",", $.identifier)), "=", $._expression),
       ),
 
-    identifier: ($) => /[a-zA-Z]+([a-zA-Z0-9_])*/,
+    _expression: ($) =>
+      choice(
+        $.block,
+        $.application,
+        $.if_statement,
+        $.lambda,
+        $.unary_expression,
+        $.binary_expression,
+        $.literal,
+      ),
 
-    literal: ($) => choice($.unit, $.number, $.boolean, $.string),
-    unit: ($) => choice("{}", "unit"),
-    number: ($) => /\d+/,
-    boolean: ($) => choice("true", "false"),
-    string: ($) => seq('"', /[^\"]/, '"'),
+    block: ($) => seq("{", repeat($._statememt), "}"),
+
+    application: ($) =>
+      prec(14, seq($._expression, "(", $.application_params, ")")),
+
+    lambda: ($) => prec(1, seq("_", $.parameter_list, $._expression)), //lambda
+
+    if_statement: ($) =>
+      prec(
+        4,
+        seq("if", $._expression, "then", $._expression, "else", $._expression),
+      ), //if then else
 
     unary_expression: ($) =>
       choice(
-        prec.left(12, seq("-", $.expression)),
-        prec.left(12, seq("!", $.expression)),
+        prec.left(12, seq("-", $._expression)),
+        prec.left(12, seq("!", $._expression)),
       ),
 
     binary_expression: ($) =>
       choice(
-        prec.left(11, seq($.expression, "^", $.expression)),
-        prec.left(10, seq($.expression, choice("*", "/", "%"), $.expression)),
-        prec.left(9, seq($.expression, choice("+", "-"), $.expression)),
+        prec.left(11, seq($._expression, "^", $._expression)),
+        prec.left(10, seq($._expression, choice("*", "/", "%"), $._expression)),
+        prec.left(9, seq($._expression, choice("+", "-"), $._expression)),
         prec.left(
           8,
-          seq($.expression, choice("<", ">", "<=", ">="), $.expression),
+          seq($._expression, choice("<", ">", "<=", ">="), $._expression),
         ),
-        prec.left(7, seq($.expression, choice("==", "!="), $.expression)),
-        prec.left(6, seq($.expression, "and", $.expression)),
-        prec.left(5, seq($.expression, "or", $.expression)),
-        prec.left(3, seq($.expression, choice(".", ","), $.expression)),
+        prec.left(7, seq($._expression, choice("==", "!="), $._expression)),
+        prec.left(6, seq($._expression, "and", $._expression)),
+        prec.left(5, seq($._expression, "or", $._expression)),
+        prec.right(3, seq($._expression, choice(".", ","), $._expression)),
       ),
 
-    deconstruct: ($) => seq($.expression, ",", $.expression),
+    literal: ($) =>
+      choice($.unit, $.number, $.boolean, $.string, $.fstring, $.identifier),
+    identifier: ($) => /[a-zA-Z]+([a-zA-Z0-9_])*/,
+    unit: ($) => choice("{}", "unit"),
+    number: ($) => /\d+/,
+    boolean: ($) => choice("true", "false"),
+    string: ($) => seq('"', /[^\"]*?/, '"'),
+    fstring: ($) => seq('f"', /[^\"]*?/, '"'),
 
-    if_statement: ($) =>
-      seq("if", $.expression, "then", $.expression, "else", $.expression), //if then else
-    lambda: ($) => seq("_", $.parameter_list, $.expression), //lambda
     comment: ($) => token(seq("//", /.*/)),
+    semicolon: ($) => ";",
   },
 });
